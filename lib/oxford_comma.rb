f8a33d0aeb1153a11a71ab2ee154951f7691a3aa
kovner chalumo <kchalumo@gmail.com>
2018-11-27 14:29:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  len = array.length
+  if len == 2 
+    str = "#{array[0]} and #{array[1]}"
+  else
+    str = array.shift
+    array.each do |word|
+      puts array.length
+      if array.length == 1 
+        str << ", and #{array.shift}"
+      elsif
+        str << ", #{array.shift}"
+      end
+    end
+  end
+  str
 end
