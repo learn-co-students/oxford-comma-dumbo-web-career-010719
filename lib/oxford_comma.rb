15c27679aa459983967a76278ed9a9775d043f98
Edwin Lin <edwinlin@gmail.com>
2018-12-09 08:07:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.length == 1
+    str += array[0]
+  elsif array.length == 2
+    str += "#{array[0]} and #{array[1]}"
+  else
+    array.each_with_index do |ele, idx|
+      if idx == 0
+        str += ele
+      elsif idx == array.length-1
+        str += ", and #{ele}"
+      else
+        str += ", #{ele}"
+      end
+    end
+  end
+  return str
 end
