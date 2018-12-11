1e6f5fadf77dfe4a6d99ba40c734b0e306568859
recursive-certificate-9133 <wjhuang0726@gmail.com>
2018-12-11 05:05:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+  
+  item_holder=[] 
+  
+  if array.length ==1 
+    
+    array[0]
+    
+  elsif array.length==2
+  
+    array.join(" and ")
+    
+  else
+    
+    array.each do |item|
+      
+      item_holder.push(item)
+      
+    end
+    
+    item_holder.slice(0..-2).join(", ")+", and #{item_holder[-1]}"
+    
+  end
+  
 end
