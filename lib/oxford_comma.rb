bb3c13f7a905afd8293ec765d10098ca805333bd
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-13 01:52:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  str = array[0]
+  i = 1
+  while i < array.length
+    if array.length == 2
+      str += " and #{array[i]}"
+    elsif i == array.length - 1
+      str += ", and #{array[i]}"
+    else
+      str += ", #{array[i]}"
+    end
+    i += 1
+  end
+  return str
 end
+#sdfsdfs
