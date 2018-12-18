b90c25d56dcc5dd216e1eb7558e8df5b074a066d
github username <github email address>
2018-12-18 16:41:05 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+# oxford_comma
 
+# returns a string without any additional fomatting
+# when given a 1-element array
+
+# adds 'and' between elements
+# when given a 2-element array
+# adds commas plus a final 'and'
+# when given a 3-element array
+# correctly formats arrays of lengths greater than three
+
+def oxford_comma(array)
+  if array.length >= 3
+    array[0..-2].join(", ") + ", and " + array[-1]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
