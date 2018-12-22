02ddb11730f368bf0cb8a5032977ce3265367a5a
zaheer <m.abbaszaheer@gmail.com>
2018-12-21 21:31:34 -0500

    oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
 
+#expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]))
+#.to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos")
+ 
+def oxford_comma(array)
+  	
+	if 	array.size==1 
+		array.join
+	elsif array.size==2
+			array.join (" and ")
+	elsif array.size>2
+       		array[-1]="and #{array[-1]}"
+	   		array.join (", ")
+	end
+
+	
 end
