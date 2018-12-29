000e4a716def9638bf4946e7eb22627eefb80f10
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 16:59:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size == 1
+    return array.join
+  end
+  
+  if array.size == 2
+    return array.join(" and ")
+  end
+  
+  if array.size == 3
+    array[0] << ", "
+    array[1] << ", and "
+    return array.join
+  end
+  
+  if array.size > 3
+    string = ""
+    last_object = array.pop
+    string = array.join(", ")
+    string << ", and "
+    string << "#{last_object}"
+   return string
+    
+  end
 end
